6028dc9237c0e28ac7f17a0787742bba9a5cb5ce
foldi Bence <foldib555@gmail.com>
2019-09-27 12:33:04 +0200

    * aukcioprojekt.csproj: oraimunka
    * Program.cs:
    * Festmeny.cs:

---
+++ aukcioprojekt/Festmeny.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using System.Linq;
+namespace aukcioprojekt
+{
+    public class Festmeny
+    {
+        public Festmeny()
+        { 
+        private string stilus;
+        private string cim;
+        private string festo;
+        int licitekSzama;
+        int legmagasabbLicit;
+        bool elkelt;
+        DateTime LegutolsoLicitIdeje;
+        DateTime most = DateTime.Now;
+
+
+
+        public string Cim { get => cim; set => cim = value; }
+        public string Festo { get => festo; set => festo = value; }
+        public string Stilus { get => stilus; set => stilus = value; }
+        public int LicitekSzama { get => licitekSzama; set => licitekSzama = value; }
+        public int LegmagasabbLicit { get => legmagasabbLicit; set => legmagasabbLicit = value; }
+        public bool Elkelt { get => elkelt; set => elkelt = value; }
+        public DateTime LegutolsoLicitIdeje1 { get => LegutolsoLicitIdeje; set => LegutolsoLicitIdeje = value; }
+
+        public override string ToString()
+        {
+            return festo + ": " + cim + "(" + stilus + "\n)"+ " meg ki ir sokmindent";
+               
+        }
+        public void Licit()
+        {
+            if (elkelt)
+            {
+                Console.WriteLine("nem tortenik semmi!!!!HIBA mar elkelt");
+            }
+            else if (licitekSzama <1)
+            {
+                legmagasabbLicit += 100;
+                licitekSzama++;
+                LegutolsoLicitIdeje = most;
+            }
+            else
+            {
+                legmagasabbLicit += legmagasabbLicit / 10;
+                licitekSzama++;
+                LegutolsoLicitIdeje = most;
+
+            }
+
+
+        }
+        public void Licit(int mertek)
+        {
+            if (mertek > 9 && mertek <101)
+            {
+                legmagasabbLicit += legmagasabbLicit / mertek;
+                licitekSzama++;
+                LegutolsoLicitIdeje = most;
+            }
+            else
+            {
+                Console.WriteLine("HIBAAAAAAAAAAAA");
+            }
+
+
+
+        }
+
+
+
+    }
+    }
+}
